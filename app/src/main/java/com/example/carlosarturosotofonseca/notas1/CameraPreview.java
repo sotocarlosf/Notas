0c4954e955efe37730c29642f97ab496a3e9f7f1
sotocarlosf <soto.carlosf@gmail.com>
2015-07-20 11:13:59 -0600

    Version con camara funcionando

---
+++ app/src/main/java/com/example/carlosarturosotofonseca/notas1/CameraPreview.java
@@ -0,0 +1,107 @@
+package com.example.carlosarturosotofonseca.notas1;
+
+/**
+ * Created by carlosarturosotofonseca on 13/07/15.
+ */
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.hardware.camera2.*;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.security.Policy;
+import java.util.List;
+
+/** A basic Camera preview class */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    public Camera mCamera;
+    List<Camera.Size> mSupportedPreviewSizes;
+    private String TAG;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (mCamera == null) {
+            mCamera = Camera.open();
+            try {
+                mCamera.setPreviewDisplay(holder);
+
+                // TODO test how much setPreviewCallbackWithBuffer is faster
+                mCamera.setPreviewCallback((Camera.PreviewCallback) this);
+            } catch (IOException e) {
+                mCamera.release();
+                mCamera = null;
+            }
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+        }
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void setCamera(Camera camera) {
+        mCamera = camera;
+        if (mCamera != null) {
+            mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
+            requestLayout();
+
+            // get Camera parameters
+            Camera.Parameters params = mCamera.getParameters();
+
+            List<String> focusModes = params.getSupportedFocusModes();
+            if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                // set the focus mode
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                // set Camera parameters
+                mCamera.setParameters(params);
+            }
+        }
+    }
+}
